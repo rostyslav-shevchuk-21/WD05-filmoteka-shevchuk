9c0e5ea3264c2d1e54b97af9b2da2f7c188f89ce
Rostyslav <shev4uk.rostik@gmail.com>
2019-01-17 19:47:50 +0200

    add photo in new film

--- models/films.php
+++ models/films.php
@@ -19,14 +19,79 @@ function films_all($link){
 
 // GETTING ALL FILMS FROM DB
 function film_new($link, $title, $genre, $year, $description){
+
+
+	if ( isset($_FILES['photo']['name']) && $_FILES['photo']['tmp_name'] != ""  ) {
+		$fileName = $_FILES["photo"]["name"];
+		$fileTmpLoc = $_FILES["photo"]["tmp_name"];
+		$fileType =  $_FILES["photo"]["type"];
+		$fileSize =  $_FILES["photo"]["size"];
+		$fileErrorMsg =  $_FILES["photo"]["error"];
+		$kaboom = explode(".", $fileName);
+		$fileExt = end($kaboom);
+
+		list($width, $height) = getimagesize($fileTmpLoc);
+		if($width < 10 || $height < 10){
+			$errors[] = 'That image has no dimensions';
+		}
+
+		$db_file_name = rand(100000000, 999999999) . "." . $fileExt;
+		
+		if($fileSize > 10485760) {
+			$errors[] = 'Your image file was larger than 10mb';
+		} else if (!preg_match("/\.(gif|jpg|png|jpeg)$/i", $fileName) ) {
+			$errors[] = 'Your image file was not jpg, jpeg, gif or png type';
+		} else if ($fileErrorMsg == 1) {
+			$errors[] = 'An unknown error occurred';
+		}
+
+		$photoFolderLocation = ROOT . 'data/films/full/';
+		$photoFolderLocationMin = ROOT . 'data/films/min/';
+		// $photoFolderLocationFull = ROOT . 'data/films/full/';
+
+		$uploadfile = $photoFolderLocation . $db_file_name;
+		$moveResult= move_uploaded_file($fileTmpLoc, $uploadfile);
+		
+		if ($moveResult != true) {
+			$errors[] = 'File upload failed';
+		}
+
+		require_once( ROOT . "/functions/image_resize_imagick.php");
+		$target_file =  $photoFolderLocation . $db_file_name;
+		$resized_file = $photoFolderLocationMin . $db_file_name;
+		$wmax = 137;
+		$hmax = 200;
+		$img = createThumbnail($target_file, $wmax, $hmax);
+		$img->writeImage($resized_file);
+
+	}
+
 		
 	// ЗАПИСЬ ДАННЫХ В БД
-	$query = "INSERT INTO films (title, genre, year, description) VALUES (
-	'". mysqli_real_escape_string($link, $title) ."',
-	'". mysqli_real_escape_string($link, $genre) ."',
-	'". mysqli_real_escape_string($link, $year) ."',
-	'". mysqli_real_escape_string($link, $description) ."'
-	)";
+if ( isset($db_file_name) ) {
+        $query = "INSERT INTO films (title, genre, year, description, photo) VALUES (
+        '". mysqli_real_escape_string($link, $title) ."',
+        '". mysqli_real_escape_string($link, $genre) ."',
+        '". mysqli_real_escape_string($link, $year) ."',
+        '". mysqli_real_escape_string($link, $description) ."',
+        '". mysqli_real_escape_string($link, $db_file_name) ."' 
+        )"; 
+
+        echo $query;
+        die();
+
+    } else {
+        $query = "INSERT INTO films (title, genre, year, description) VALUES (
+        '". mysqli_real_escape_string($link, $title) ."',
+        '". mysqli_real_escape_string($link, $genre) ."',
+        '". mysqli_real_escape_string($link, $year) ."',
+        '". mysqli_real_escape_string($link, $description) ."',
+        )"; 
+
+        echo $query;
+        die();
+
+    }
 
 	if ( mysqli_query($link, $query) ) {
 		$result = true;
